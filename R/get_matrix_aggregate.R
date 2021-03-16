3eecd5594bc2325ad3df2efaa9a36b57be161301
Timothy Wong <timothy.wong@hotmail.co.uk>
2021-03-16 16:56:46 +0000

    Added functions get_matrix_aggregate() and get_vertex_attr_aggregate()

---
+++ R/get_matrix_aggregate.R
@@ -0,0 +1,69 @@
+#' @title Compute Aggregate of Matrix Values
+#' @description Calculate the group by aggregate of a matrix
+#' @param g An `igraph` object
+#' @param m A matrix of numeric values. The number of column and rows must be identical. All rows and columns must be named. Ideally this is a `N*N` shortest path distance matrix.
+#' @param groups Vector of character or factor, which indicate the groups to be aggregated
+#' @param func Aggregation function
+#' @param ... Optional arguments to be passed onto the aggregation function
+#' @return A vector or matrix object
+#' @author Timothy Wong, \email{timothy.wong@@hotmail.co.uk}
+#' @examples
+#' # Use the postcode sector to derive the area
+#' # Example: BS = Bristol; BA = Bath
+#' library(stringr)
+#' area <- factor(str_extract(vertex_attr(BristolBathGraph, "name"),"^[A-Z]*"),
+#'                labels = c(BS="Bristol", BA="Bath"))
+#' 
+#' # Calculate the shortest paths for all vertexes
+#' my_shortest_paths <- distances(graph = BristolBathGraph,
+#'                                weights = edge_attr(BristolBathGraph,"duration"))
+#' 
+#' # Calculate the maximum travel time between any two vertexes within a postcode area
+#' # Returns a vector object of length 2
+#' # The names represent postcode area
+#' # The value represent the maximum travel duration of each postcode area
+#' get_matrix_aggregate(g = BristolBathGraph, 
+#'                      m = my_quickest_paths,
+#'                      groups = area,
+#'                      func = max)
+#' 
+#' # Optional arguments can be passed onto `func` using `...`.
+#' # This will calculate the percentiles of the travel duration of each postcode area
+#' # Returns a matrix object of 2 columns * 5 rows.
+#' # The columns represent postcode areas
+#' # The rows are names as '10%', '30%', '50%', '70%' and '90%'
+#' # You may use `names=FALSE` to suppress row names (see documentation for `?quantile()`)
+#' get_matrix_aggregate(g = BristolBathGraph, 
+#'                      m = my_quickest_paths,
+#'                      groups = area,
+#'                      func = quantile,
+#'                      probs = c(0.1, 0.3, 0.5, 0.7, 0.9))
+#' 
+#' get_matrix_aggregate(g = BristolBathGraph, 
+#'                      m = my_quickest_paths,
+#'                      groups = area,
+#'                      func = quantile,
+#'                      probs = c(0.1, 0.3, 0.5, 0.7, 0.9),
+#'                      names = FALSE)
+#' @export
+get_matrix_aggregate <- function(g,
+                                 m,
+                                 groups,
+                                 func,
+                                    ...) {
+  
+  if (nrow(m) != ncol(m)) {
+    stop("Matrix m needs identical number of rows and columns")
+  }
+  
+  if (!all(colnames(m) == rownames(m))){
+    stop("Matrix m needs identical colnames and rownames")
+  }
+  
+  sapply(as.character(unique(groups)), function(gp) {
+    func(m
+         [vertex_attr(g, "name"), vertex_attr(g, "name")]
+         [groups == gp, groups == gp], 
+         ...) },
+    USE.NAMES = TRUE)
+}
---
+++ R/get_vertex_attr_aggregate.R
@@ -0,0 +1,61 @@
+#' @title Compute Aggregate of Vertex Attributes
+#' @description Calculate the group by aggregate of a specific vertex attribute
+#' @param g An `igraph` object
+#' @param attr Character value, indicating the vertex attribute to be aggregated
+#' @param groups Vector of character or factor, which indicate the groups to be aggregated
+#' @param func Aggregation function
+#' @param ... Optional arguments to be passed onto the aggregation function
+#' @return A vector or matrix object
+#' @author Timothy Wong, \email{timothy.wong@@hotmail.co.uk}
+#' @examples
+#' # Use the postcode sector to derive the area
+#' # Example: BS = Bristol; BA = Bath
+#' library(stringr)
+#' area <- factor(str_extract(vertex_attr(BristolBathGraph, "name"),"^[A-Z]*"),
+#'                labels = c(BS="Bristol", BA="Bath"))
+#' 
+#' # Calculate the aggregate of vertex attribute
+#' # Returns a vector object of length 2
+#' # The names represent postcode area
+#' # The value represent total population of each postcode area
+#' get_vertex_attr_aggregate(g = BristolBathGraph,
+#'                           groups = area,
+#'                           attr = "population", 
+#'                           func = sum)
+#' 
+#' # Optional arguments can be passed onto `func` using `...`.
+#' # This will calculate the percentiles of the population of each postcode area
+#' # Returns a matrix object of 2 columns * 4 rows.
+#' # The columns represent postcode areas
+#' # The rows are names as '20%', '40%', '60%' and '80%'
+#' # You may use `names=FALSE` to suppress row names (see documentation for `?quantile()`)
+#' get_vertex_attr_aggregate(g = BristolBathGraph,
+#'                           groups = area,
+#'                           attr = "population", 
+#'                           func = quantile,
+#'                           probs = c(0.2, 0.4, 0.6, 0.8))
+#' 
+#' get_vertex_attr_aggregate(g = BristolBathGraph,
+#'                           groups = area,
+#'                           attr = "population", 
+#'                           func = quantile,
+#'                           probs = c(0.2, 0.4, 0.6, 0.8),
+#'                           names = FALSE)
+#' @export
+get_vertex_attr_aggregate <- function(g, 
+                                      attr, 
+                                      groups,
+                                      func,
+                                      ...) {
+  values <- igraph::vertex_attr(g, attr)
+  by <- list(group = groups)
+  
+  if (length(by$group) != length(values)) {
+    stop("Length of attributes and length of groups are not identical")
+  }
+  
+  sapply(as.character(unique(by$group)),
+         function(gp){func(values[gp==by$group],...)},
+         USE.NAMES = TRUE)
+  
+}
